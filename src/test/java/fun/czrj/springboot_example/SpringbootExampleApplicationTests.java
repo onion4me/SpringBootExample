a21dcadbb6f02fa864e4ca27680a0598cc097670
九门提督 <zhangjian@czrj.fun>
2020-04-12 00:22:25 +0800

    2020-04-12 springboot 基础结构提交

---
+++ src/test/java/fun/czrj/springboot_example/SpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.czrj.springboot_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
